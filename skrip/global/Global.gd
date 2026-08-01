7d89fa3ec5cf842c138e432f573e3b6de0acea5e
azizul987 <wawanajah954@gmail.com>
2026-08-02 06:12:16 +0700

    add global skrip

---
+++ skrip/global/Global.gd
@@ -0,0 +1,7 @@
+extends Node
+
+var current_level: int = 1
+var checkpoint_position: Vector2 = Vector2.ZERO
+var current_rule: int = 0
+var gravity_direction: Vector2 = Vector2.DOWN
+var death_count: int = 0
