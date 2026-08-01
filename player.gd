a56b83c26f396cd21344252b3a2a009b42a77410
azizul987 <wawanajah954@gmail.com>
2026-08-02 06:03:59 +0700

    add movement  skrip

---
+++ player.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+const SPEED = 200.0
+const JUMP_POWER = -400.0
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_POWER
+
+	var direction = Input.get_axis("ui_left", "ui_right")
+
+	if direction != 0:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = 0
+
+	move_and_slide()
