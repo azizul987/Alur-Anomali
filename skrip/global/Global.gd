e78a6f343f460017566d3164b572fb415a5376be
azizul987 <wawanajah954@gmail.com>
2026-08-02 07:39:21 +0700

    add garvity dir

--- skrip/global/Global.gd
+++ skrip/global/Global.gd
@@ -3,5 +3,5 @@ extends Node
 var current_level: int = 1
 var checkpoint_position: Vector2 = Vector2.ZERO
 var current_rule: int = 0
-var gravity_direction: Vector2 = Vector2.DOWN
+var gravity_direction: Vector2 = Vector2.UP
 var death_count: int = 0
--- skrip/player.gd
+++ skrip/player.gd
@@ -1,28 +1,34 @@
 extends CharacterBody2D
 
 const SPEED = 200.0
-const JUMP_POWER = -400.0
+const JUMP_POWER = 400.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-func  _ready() -> void:
+func _ready() -> void:
 	SaveManager.load_game()
-	global_position=Global.checkpoint_position
+	global_position = Global.checkpoint_position
 
 func _physics_process(delta):
+	up_direction = -Global.gravity_direction
+
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		velocity += gravity * delta * Global.gravity_direction
 
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_POWER
+		velocity -= Global.gravity_direction * JUMP_POWER
 
 	var direction = Input.get_axis("ui_left", "ui_right")
 
-	if direction != 0:
-		velocity.x = direction * SPEED
+	# Gravitasi kiri atau kanan.
+	if Global.gravity_direction.x != 0:
+		velocity.y = direction * SPEED
+
+	# Gravitasi atas atau bawah.
 	else:
-		velocity.x = 0
+		velocity.x = direction * SPEED
 
 	move_and_slide()
-	Global.checkpoint_position=global_position
+
+	Global.checkpoint_position = global_position
 	SaveManager.save_game()
